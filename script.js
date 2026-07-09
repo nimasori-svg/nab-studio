eb494e3a23f260676c23361b62e356e5c92cd625
nimasori-svg <nimasori551@gmail.com>
2026-07-09 23:39:52 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const menu=document.querySelector(".menu");
+
+const sideMenu=document.querySelector(".side-menu");
+
+const overlay=document.querySelector(".overlay");
+
+const closeBtn=document.querySelector(".close-btn");
+
+menu.onclick=()=>{
+
+sideMenu.classList.add("active");
+
+overlay.classList.add("active");
+
+}
+
+closeBtn.onclick=()=>{
+
+sideMenu.classList.remove("active");
+
+overlay.classList.remove("active");
+
+}
+
+overlay.onclick=()=>{
+
+sideMenu.classList.remove("active");
+
+overlay.classList.remove("active");
+
+}
+
+const gameCards = document.querySelectorAll(".game-card");
+
+window.addEventListener("scroll", () => {
+
+    gameCards.forEach(card => {
+
+        const top = card.getBoundingClientRect().top;
+
+        if (top < window.innerHeight - 100) {
+
+            card.classList.add("show");
+
+        }
+
+    });
+
+});
